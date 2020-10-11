2f4ab08dbf49ec848f32cac2d1b5f2727a08516e
Dheepak Krishnamurthy <me@kdheepak.com>
2020-10-10 18:58:01 -0600

    Refactor more code to use task_report_table

--- src/app.rs
+++ src/app.rs
@@ -133,6 +133,7 @@ pub enum AppMode {
 pub struct TaskReportTable {
     pub labels: Vec<String>,
     pub columns: Vec<String>,
+    pub tasks: Vec<Vec<String>>,
 }
 
 impl TaskReportTable {
@@ -140,9 +141,173 @@ impl TaskReportTable {
         let task_report_table = Self {
             labels: vec![],
             columns: vec![],
+            tasks: vec![vec![]],
         };
         task_report_table
     }
+
+    pub fn export_headers(&mut self) {
+        self.columns = vec![];
+        self.labels = vec![];
+
+        let output = Command::new("task")
+            .arg("show")
+            .arg("report.next.columns")
+            .output()
+            .expect("Unable to run `task show report.next.columns`. Check documentation for more information");
+        let data = String::from_utf8(output.stdout).unwrap();
+
+        for line in data.split('\n') {
+            if line.starts_with("report.next.columns") {
+                let column_names: &str = line.split(' ').collect::<Vec<&str>>()[1];
+                for column in column_names.split(',') {
+                    self.columns.push(column.to_string());
+                }
+            }
+        }
+
+        let output = Command::new("task")
+            .arg("show")
+            .arg("report.next.labels")
+            .output()
+            .expect("Unable to run `task show report.next.labels`. Check documentation for more information");
+        let data = String::from_utf8(output.stdout).unwrap();
+
+        for line in data.split('\n') {
+            if line.starts_with("report.next.labels") {
+                let label_names: &str = line.split(' ').collect::<Vec<&str>>()[1];
+                for label in label_names.split(',') {
+                    self.labels.push(label.to_string());
+                }
+            }
+        }
+    }
+
+    pub fn generate_table(&mut self, tasks: &Vec<Task>) {
+
+        self.tasks = vec![];
+
+        // get all tasks as their string representation
+        for task in tasks {
+            let mut item = vec![];
+            for name in &self.columns {
+                let s = self.get_string_attribute(name, &task);
+                item.push(s);
+            }
+            self.tasks.push(item)
+        }
+
+    }
+
+    pub fn get_string_attribute(&self, attribute: &str, task: &Task) -> String {
+        let tags = vec![
+            "PROJECT",
+            "BLOCKED",
+            "UNBLOCKED",
+            "BLOCKING",
+            "DUE",
+            "DUETODAY",
+            "TODAY",
+            "OVERDUE",
+            "WEEK",
+            "MONTH",
+            "QUARTER",
+            "YEAR",
+            "ACTIVE",
+            "SCHEDULED",
+            "PARENT",
+            "CHILD",
+            "UNTIL",
+            "WAITING",
+            "ANNOTATED",
+            "READY",
+            "YESTERDAY",
+            "TOMORROW",
+            "TAGGED",
+            "PENDING",
+            "COMPLETED",
+            "DELETED",
+            "UDA",
+            "ORPHAN",
+            "PRIORITY",
+            "PROJECT",
+            "LATEST",
+        ];
+
+        match attribute {
+            "id" => task.id().unwrap_or_default().to_string(),
+            "due.relative" => match task.due() {
+                Some(v) => vague_format_date_time(
+                    Local::now().naive_utc(),
+                    NaiveDateTime::new(v.date(), v.time()),
+                ),
+                None => "".to_string(),
+            },
+            "entry.age" => vague_format_date_time(
+                NaiveDateTime::new(task.entry().date(), task.entry().time()),
+                Local::now().naive_utc(),
+            ),
+            "start.age" => match task.start() {
+                Some(v) => vague_format_date_time(
+                    NaiveDateTime::new(v.date(), v.time()),
+                    Local::now().naive_utc(),
+                ),
+                None => "".to_string(),
+            },
+            "project" => match task.project() {
+                Some(p) => format!("{}", p).to_string(),
+                None => "".to_string(),
+            },
+            "depends.count" => match task.depends() {
+                Some(v) => {
+                    if v.len() == 0 {
+                        "".to_string()
+                    } else {
+                        format!("{}", v.len()).to_string()
+                    }
+                }
+                None => "".to_string(),
+            },
+            "tags.count" => match task.tags() {
+                Some(v) => {
+                    let t = v
+                        .into_iter()
+                        .filter(|t| !tags.contains(&t.as_str()))
+                        .cloned()
+                        .collect::<Vec<String>>()
+                        .len();
+                    if t == 0 {
+                        "".to_string()
+                    } else {
+                        format!("{}", t).to_string()
+                    }
+                }
+                None => "".to_string(),
+            },
+            "tags" => match task.tags() {
+                Some(v) => {
+                    let t = v
+                        .iter()
+                        .filter(|t| !tags.contains(&t.as_str()))
+                        .cloned()
+                        .collect::<Vec<String>>()
+                        .join(",");
+                    format!("{}", t).to_string()
+                }
+                None => "".to_string(),
+            },
+            "description.count" => task.description().to_string(),
+            "description" => task.description().to_string(),
+            "urgency" => match &task.uda()["urgency"] {
+                UDAValue::Str(_) => "0.00".to_string(),
+                UDAValue::U64(u) => format!("{:.2}", *u as f64).to_string(),
+                UDAValue::F64(f) => format!("{:.2}", *f).to_string(),
+            },
+            _ => "".to_string(),
+        }
+    }
+
+
 }
 
 pub struct TTApp {
@@ -616,137 +781,21 @@ impl TTApp {
         f.render_stateful_widget(t, rect, &mut self.state);
     }
 
-    pub fn get_string_attribute(&self, attribute: &str, task: &Task) -> String {
-        let tags = vec![
-            "PROJECT",
-            "BLOCKED",
-            "UNBLOCKED",
-            "BLOCKING",
-            "DUE",
-            "DUETODAY",
-            "TODAY",
-            "OVERDUE",
-            "WEEK",
-            "MONTH",
-            "QUARTER",
-            "YEAR",
-            "ACTIVE",
-            "SCHEDULED",
-            "PARENT",
-            "CHILD",
-            "UNTIL",
-            "WAITING",
-            "ANNOTATED",
-            "READY",
-            "YESTERDAY",
-            "TOMORROW",
-            "TAGGED",
-            "PENDING",
-            "COMPLETED",
-            "DELETED",
-            "UDA",
-            "ORPHAN",
-            "PRIORITY",
-            "PROJECT",
-            "LATEST",
-        ];
-
-        match attribute {
-            "id" => task.id().unwrap_or_default().to_string(),
-            "due.relative" => match task.due() {
-                Some(v) => vague_format_date_time(
-                    Local::now().naive_utc(),
-                    NaiveDateTime::new(v.date(), v.time()),
-                ),
-                None => "".to_string(),
-            },
-            "entry.age" => vague_format_date_time(
-                NaiveDateTime::new(task.entry().date(), task.entry().time()),
-                Local::now().naive_utc(),
-            ),
-            "start.age" => match task.start() {
-                Some(v) => vague_format_date_time(
-                    NaiveDateTime::new(v.date(), v.time()),
-                    Local::now().naive_utc(),
-                ),
-                None => "".to_string(),
-            },
-            "project" => match task.project() {
-                Some(p) => format!("{}", p).to_string(),
-                None => "".to_string(),
-            },
-            "depends.count" => match task.depends() {
-                Some(v) => {
-                    if v.len() == 0 {
-                        "".to_string()
-                    } else {
-                        format!("{}", v.len()).to_string()
-                    }
-                }
-                None => "".to_string(),
-            },
-            "tags.count" => match task.tags() {
-                Some(v) => {
-                    let t = v
-                        .into_iter()
-                        .filter(|t| !tags.contains(&t.as_str()))
-                        .cloned()
-                        .collect::<Vec<String>>()
-                        .len();
-                    if t == 0 {
-                        "".to_string()
-                    } else {
-                        format!("{}", t).to_string()
-                    }
-                }
-                None => "".to_string(),
-            },
-            "tags" => match task.tags() {
-                Some(v) => {
-                    let t = v
-                        .iter()
-                        .filter(|t| !tags.contains(&t.as_str()))
-                        .cloned()
-                        .collect::<Vec<String>>()
-                        .join(",");
-                    format!("{}", t).to_string()
-                }
-                None => "".to_string(),
-            },
-            "description.count" => task.description().to_string(),
-            "description" => task.description().to_string(),
-            "urgency" => match &task.uda()["urgency"] {
-                UDAValue::Str(_) => "0.00".to_string(),
-                UDAValue::U64(u) => format!("{:.2}", *u as f64).to_string(),
-                UDAValue::F64(f) => format!("{:.2}", *f).to_string(),
-            },
-            _ => "".to_string(),
-        }
-    }
-
     pub fn task_report(&mut self) -> (Vec<Vec<String>>, Vec<String>, Vec<i16>) {
-        let mut alltasks = vec![];
+        let alltasks = &*(self.tasks.lock().unwrap());
 
-        // get all tasks as their string representation
-        for task in &*(self.tasks.lock().unwrap()) {
-            let mut item = vec![];
-            for name in &self.task_report_table.columns {
-                let s = self.get_string_attribute(name, &task);
-                item.push(s);
-            }
-            alltasks.push(item)
-        }
+        self.task_report_table.generate_table(alltasks);
 
         // find which columns are empty
         let null_columns_len;
-        if !alltasks.is_empty() {
-            null_columns_len = alltasks[0].len();
+        if !self.task_report_table.tasks.is_empty() {
+            null_columns_len = self.task_report_table.tasks[0].len();
         } else {
             return (vec![], vec![], vec![]);
         }
 
         let mut null_columns = vec![0; null_columns_len];
-        for task in &alltasks {
+        for task in &self.task_report_table.tasks {
             for (i, s) in task.iter().enumerate() {
                 null_columns[i] += s.len();
             }
@@ -754,7 +803,7 @@ impl TTApp {
 
         // filter out columns where everything is empty
         let mut tasks = vec![];
-        for task in &alltasks {
+        for task in &self.task_report_table.tasks {
             let t = task.clone();
             let t: Vec<String> = t
                 .iter()
@@ -791,7 +840,7 @@ impl TTApp {
     pub fn update(&mut self) {
         self.export_tasks();
         self.update_tags();
-        self.export_headers();
+        self.task_report_table.export_headers();
     }
 
     pub fn next(&mut self) {
@@ -825,43 +874,6 @@ impl TTApp {
             None => 0,
         };
         self.state.select(Some(i));
-    }
-
-    pub fn export_headers(&mut self) {
-        self.task_report_table.columns = vec![];
-        self.task_report_table.labels = vec![];
-
-        let output = Command::new("task")
-            .arg("show")
-            .arg("report.next.columns")
-            .output()
-            .expect("Unable to run `task show report.next.columns`. Check documentation for more information");
-        let data = String::from_utf8(output.stdout).unwrap();
-
-        for line in data.split('\n') {
-            if line.starts_with("report.next.columns") {
-                let column_names: &str = line.split(' ').collect::<Vec<&str>>()[1];
-                for column in column_names.split(',') {
-                    self.task_report_table.columns.push(column.to_string());
-                }
-            }
-        }
-
-        let output = Command::new("task")
-            .arg("show")
-            .arg("report.next.labels")
-            .output()
-            .expect("Unable to run `task show report.next.labels`. Check documentation for more information");
-        let data = String::from_utf8(output.stdout).unwrap();
-
-        for line in data.split('\n') {
-            if line.starts_with("report.next.labels") {
-                let label_names: &str = line.split(' ').collect::<Vec<&str>>()[1];
-                for label in label_names.split(',') {
-                    self.task_report_table.labels.push(label.to_string());
-                }
-            }
-        }
     }
 
     pub fn export_tasks(&self) {
