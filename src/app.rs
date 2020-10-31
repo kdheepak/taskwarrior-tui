2adfc04a14b83c34b34729348fd455969d063c65
Dheepak Krishnamurthy <me@kdheepak.com>
2020-10-30 23:01:44 -0600

    Call export_headers every second

--- src/app.rs
+++ src/app.rs
@@ -575,6 +575,7 @@ impl TTApp {
     }
 
     pub fn update(&mut self) -> Result<(), Box<dyn Error>> {
+        self.task_report_table.export_headers()?;
         self.export_tasks()?;
         self.update_tags();
         Ok(())
