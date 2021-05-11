54e4cce0841d670aeb9d5631bb4e614dc688b4ce
Dheepak Krishnamurthy <me@kdheepak.com>
2021-05-11 11:14:03 -0600

    Add completion

---
+++ completions/taskwarrior-tui.fish
@@ -0,0 +1,4 @@
+complete -c taskwarrior-tui -n "__fish_use_subcommand" -s c -l config -d 'Sets a custom config file' -r
+complete -c taskwarrior-tui -n "__fish_use_subcommand" -s r -l report -d 'Sets default report' -r
+complete -c taskwarrior-tui -n "__fish_use_subcommand" -s h -l help -d 'Prints help information'
+complete -c taskwarrior-tui -n "__fish_use_subcommand" -s V -l version -d 'Prints version information'
