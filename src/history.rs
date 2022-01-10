669a6a719f6b53e9f18692ea652bcbdd76aeb896
Dheepak Krishnamurthy <me@kdheepak.com>
2022-01-10 13:36:28 -0700

    fix: Use data instead of config 🐛

--- src/history.rs
+++ src/history.rs
@@ -18,7 +18,7 @@ impl HistoryContext {
         let data_path = if let Ok(s) = std::env::var("TASKWARRIOR_TUI_DATA") {
             PathBuf::from(s)
         } else {
-            dirs::config_dir()
+            dirs::data_dir()
                 .map(|d| d.join("taskwarrior-tui"))
                 .expect("Unable to create configuration directory for taskwarrior-tui")
         };
